220425d7b3359e41aab442e4360a09eecb8cfa6f
tyshi <perfectboy666@outlook.com>
2021-09-03 22:21:22 +0800

    fix bugs

--- dang-jian-master/zunyi/src/pages/ch3/sec33/preview.tsx
+++ dang-jian-master/zunyi/src/pages/ch3/sec33/preview.tsx
@@ -1,5 +1,5 @@
 import * as Antd from "antd";
-import sec from "./sec.png"
+import sec33 from "./sec33.png"
 export default () => (
   <Antd.Typography>
     <Antd.Typography.Title level={2}>北约袭击中国驻南斯拉夫大使馆</Antd.Typography.Title>
@@ -8,7 +8,7 @@ export default () => (
         <p>   以美国为首的北约飞机用导弹袭击中国驻南斯拉夫大使馆，造成馆舍破坏和人员伤亡。</p>
       </div>
       <div style={{float:"left"}}>
-        <img src={sec} style={{width:"100px" ,height:"60px"}}/>
+        <img src={sec33} style={{width:"100px" ,height:"60px"}}/>
       </div>
     </Antd.Typography.Text>
   </Antd.Typography>
